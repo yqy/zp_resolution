65de601e3271003102fdf70431373cbe6465a239
yqy <200805247@qq.com>
2016-11-13 18:27:56 +0800

    s

--- log_analysis.py
+++ log_analysis.py
@@ -9,4 +9,26 @@ while True:
     if line == "Begin test":
         ## new test ##
         echo_num += 1
+        print "Turn,"echo_num
+
+        zp_num = 0
+        while True:
+            line = log_file.readline()
+            if not line:break
+            line = line.strip()
+            if line.startswith("------"):
+                zp_num += 1
+                while True:
+                    line = log_file.readline()
+                    if not line:break
+                    line = line.strip()
+                    if line.startswith("Done ZP"):
+                        break 
+
+            if line.endswith("seconds!"):
+                print zp_num
+                ### out ###
+                break
+
+
 print echo_num
